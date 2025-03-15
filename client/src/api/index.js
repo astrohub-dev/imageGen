c330d677fe02b622d906c820752c0c5ab9ce7aee
Astrohub <ndujiovita1@gmail.com>
2025-03-15 16:28:21 +0100

    added netlify

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api/"
+    baseURL: "https://imagegen-hoqm.onrender.com/api/"
 });
 
 export const GetPosts = async () => await API.get("/post/");
